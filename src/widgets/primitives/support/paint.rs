9c3fb29b9dbd06dc9e6b0bc229795ecef63674dc
PORTALSURFER <portalsurfer@gmail.com>
2026-05-10 01:07:07 +0200

    Move card paint into card module

--- src/widgets/primitives/support/paint.rs
+++ src/widgets/primitives/support/paint.rs
@@ -8,7 +8,7 @@ pub(in crate::widgets::primitives) use chrome::{
     push_button_chrome, push_checkbox_chrome, push_control_chrome,
 };
 pub(in crate::widgets::primitives) use controls::{
-    push_card_widget_paint, push_drag_handle_widget_paint, push_list_item_widget_paint,
-    push_selectable_widget_paint, push_text_widget_paint,
+    push_drag_handle_widget_paint, push_list_item_widget_paint, push_selectable_widget_paint,
+    push_text_widget_paint,
 };
 pub(in crate::widgets::primitives) use media::{push_canvas_widget_paint, push_image_widget_paint};
--- src/widgets/primitives/support/paint/controls.rs
+++ src/widgets/primitives/support/paint/controls.rs
@@ -1,10 +1,8 @@
-mod card;
 mod drag_handle;
 mod list_item;
 mod selectable;
 mod text;
 
-pub(in crate::widgets::primitives) use card::push_card_widget_paint;
 pub(in crate::widgets::primitives) use drag_handle::push_drag_handle_widget_paint;
 pub(in crate::widgets::primitives) use list_item::push_list_item_widget_paint;
 pub(in crate::widgets::primitives) use selectable::push_selectable_widget_paint;

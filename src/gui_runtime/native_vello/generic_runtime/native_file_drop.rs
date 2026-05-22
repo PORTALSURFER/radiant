250ec1127b47ac50c6fc9afead1e3e884fcbbbf0
PORTALSURFER <portalsurfer@gmail.com>
2026-05-22 18:58:38 +0200

    Log native file drop routing

--- src/gui_runtime/native_vello/generic_runtime/native_file_drop.rs
+++ src/gui_runtime/native_vello/generic_runtime/native_file_drop.rs
@@ -10,6 +10,12 @@ where
     Bridge: RuntimeBridge<Message>,
 {
     pub(super) fn handle_native_file_hover(&mut self, event_loop: &ActiveEventLoop, path: PathBuf) {
+        tracing::info!(
+            path = %path.display(),
+            position = ?self.last_cursor,
+            target_widget = ?self.native_file_drop_target(),
+            "radiant generic native vello: native file hover"
+        );
         self.dispatch_native_file_drop_event(
             event_loop,
             NativeFileDrop::hover(path, self.last_cursor, self.native_file_drop_target()),
@@ -17,6 +23,11 @@ where
     }
 
     pub(super) fn handle_native_file_cancel(&mut self, event_loop: &ActiveEventLoop) {
+        tracing::info!(
+            position = ?self.last_cursor,
+            target_widget = ?self.native_file_drop_target(),
+            "radiant generic native vello: native file hover cancelled"
+        );
         self.dispatch_native_file_drop_event(
             event_loop,
             NativeFileDrop::cancel(self.last_cursor, self.native_file_drop_target()),
@@ -24,6 +35,12 @@ where
     }
 
     pub(super) fn handle_native_file_drop(&mut self, event_loop: &ActiveEventLoop, path: PathBuf) {
+        tracing::info!(
+            path = %path.display(),
+            position = ?self.last_cursor,
+            target_widget = ?self.native_file_drop_target(),
+            "radiant generic native vello: native file dropped"
+        );
         self.dispatch_native_file_drop_event(
             event_loop,
             NativeFileDrop::dropped(path, self.last_cursor, self.native_file_drop_target()),

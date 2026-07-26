d56c86abd9c639bce191b67a33dc144bcc8e2425
PORTALSURFER <portalsurfer@gmail.com>
2026-07-26 14:53:55 +0200

    style: format rebased mapper exports

--- src/prelude/application/surfaces.rs
+++ src/prelude/application/surfaces.rs
@@ -2,9 +2,8 @@
 
 pub use crate::application::{
     DynamicWidget, DynamicWidgetLocalParts, DynamicWidgetParts, FeedbackOverlayBuilder,
-    PanelSectionGeometry, PropertyRow, ScrollbarBuilder,
-    closeable_dialog_layer, custom_widget, custom_widget_direct, custom_widget_mapped,
-    dialog_layer, image, panel_section, panel_section_from_header_parts,
-    panel_section_resize_header, render_canvas, render_canvas_input,
-    render_canvas_with_capabilities, scrollbar, status_bar,
+    PanelSectionGeometry, PropertyRow, ScrollbarBuilder, closeable_dialog_layer, custom_widget,
+    custom_widget_direct, custom_widget_mapped, dialog_layer, image, panel_section,
+    panel_section_from_header_parts, panel_section_resize_header, render_canvas,
+    render_canvas_input, render_canvas_with_capabilities, scrollbar, status_bar,
 };

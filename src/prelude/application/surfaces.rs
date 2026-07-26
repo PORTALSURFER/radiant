f67fd2829570d99f6922d7b5acabd27e9fc584e6
PORTALSURFER <portalsurfer@gmail.com>
2026-07-26 14:44:33 +0200

    fix: expose local mapper parts in prelude

--- src/prelude/application/surfaces.rs
+++ src/prelude/application/surfaces.rs
@@ -1,7 +1,8 @@
 //! Custom surface and media-builder prelude exports.
 
 pub use crate::application::{
-    DynamicWidget, FeedbackOverlayBuilder, PanelSectionGeometry, PropertyRow, ScrollbarBuilder,
+    DynamicWidget, DynamicWidgetLocalParts, DynamicWidgetParts, FeedbackOverlayBuilder,
+    PanelSectionGeometry, PropertyRow, ScrollbarBuilder,
     closeable_dialog_layer, custom_widget, custom_widget_direct, custom_widget_mapped,
     dialog_layer, image, panel_section, panel_section_from_header_parts,
     panel_section_resize_header, render_canvas, render_canvas_input,
